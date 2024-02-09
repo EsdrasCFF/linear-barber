01e784c33b6ba45fe2043ebfaa4d80e9058ea65a
EsdrasCFF <ecff.dev@gmail.com>
2024-02-08 22:57:48 -0300

    style: header page styling

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import { Header } from '@/components/Header'
-import Image from 'next/image'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 export default function Home() {
   return (
     <div>
       <Header/>
+
+      <div className='px-5 pt-5' >
+        <h2 className='text-xl font-bold' >Olá, Esdras</h2>
+        
+        <p className='capitalize' >
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {locale: ptBR})}
+        </p>
+      </div>
     </div>
   )
 }
