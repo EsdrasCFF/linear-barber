05eae743a812313a534f9d419d28708643503f2f
EsdrasCFF <ecff.dev@gmail.com>
2024-02-11 12:19:44 -0300

    feat: add nextAuth provider

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { AuthOptions } from "next-auth"
+import { database } from "./prisma"
+import GoogleProvider from "next-auth/providers/google"
+import { Adapter } from "next-auth/adapters";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(database) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ]
+};
