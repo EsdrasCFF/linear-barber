7cc82155694538cf2b591c4eebd2177918cfc3b4
EsdrasCFF <ecff.dev@gmail.com>
2024-02-12 12:08:44 -0300

    feat: add AuthProvider and setup googleProvider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
+"use client"
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 
 export function Header() {
+  
   return (
     <Card>
       <CardContent className="p-5 flex justify-between items-center" >
         <Image src="/logo.png" width={105} height={20} alt="logo"/>
         <Button variant="outline" size="icon" className="w-8 h-8"> <MenuIcon size={18} /> </Button>
+
       </CardContent>
     </Card>
   )
