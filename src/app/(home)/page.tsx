d556e7b6e217c4cf2881c93369b828ddbe62f1ce
EsdrasCFF <ecff.dev@gmail.com>
2024-02-15 22:46:01 -0300

    feat: add user to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -31,7 +31,9 @@ export default async function Home() {
       <Header/>
 
       <div className='px-5 pt-5' >
-        <h2 className='text-xl font-bold' >Olá, Esdras</h2>
+        <h2 className='text-xl font-bold' >
+          {session?.user ? `Olá, ${session.user.name.split(' ')[0]}` : "Olá, Faça login e agende seu atendimento!"}
+        </h2>
         
         <p className='capitalize' >
           {format(new Date(), "EEEE',' dd 'de' MMMM", {locale: ptBR})}
